65269ba6b90371eca92b2d0708217f8778e8bf96
yuzbamaria <54204342+yuzbamaria@users.noreply.github.com>
2023-11-30 14:29:44 +0000

    added code to function getPasswordOptions()

--- script.js
+++ script.js
@@ -89,19 +89,56 @@ var upperCasedCharacters = [
 ];
 
 // declare variables globally for password object itself and each property&value
-
+// objects key value pairs
+let passwordLength;
+let includeSpecialCharacters;
+let includeNumbers;
+let includeUpperCasedCharacters;
+let includeLowerCasedCharacters;
+// return object
+let userPasswordOptions;
 // Function to prompt user for password options
-function getPasswordOptions() {
 
+
+function getPasswordOptions() {
 // Present a series of prompts for password criteria
 // Prompt user to choose the desired password length – at least 8 characters but no more than 128.
-// If statement: if the length is less or more the required one, alert user about the required length 
+passwordLength = prompt("Hi there🙂! You can generate a password with my help. Please write your desired password length. It can be between 8 and 128 characters.");
+
+// Validate user input for length 
+//If statement: if the length is less or more the required one, alert user about the required length 
+if (passwordLength < 8 || passwordLength > 128) {
+  alert("Sorry, your password should be between 8 and 128 characters long. Try again.");
+  passwordLength = prompt("Please enter a valid password length between 8 and 128 characters.");
+  // return;
+};
+
 // Use confirm 4 times so that user can confirm whether they want to include 4 types of characters
+  includeSpecialCharacters = confirm("Would you like to include special characters in your password? For example, @, %, +, /, ', !, etc.");
+  includeNumbers = confirm("Would you like to include include numbers in your password? For example, 0, 1, 2, 3, 4, etc.");
+  includeUpperCasedCharacters = confirm("Would you like to include lowercased characters? For example. a, b, c, d, etc.");
+  includeLowerCasedCharacters = confirm("Would you like to include uppercased characters? For example, A, B, C, D, etc.");
+
 // If statement: if user hasn't chosen any characters type, use alert to guide user to choose any type
-// Function `getPasswordOptions` returns an object (return statement) with user choices: length & 4 character types 
-
-
+if (!(includeSpecialCharacters || includeNumbers || includeUpperCasedCharacters || includeLowerCasedCharacters)) {
+  alert("Please choose one option at least.");
+  includeSpecialCharacters = confirm("Would you like to include special characters in your password? For example, @, %, +, /, ', !, etc.");
+  includeNumbers = confirm("Would you like to include include numbers in your password? For example, 0, 1, 2, 3, 4, etc.");
+  includeUpperCasedCharacters = confirm("Would you like to include lowercased characters? For example. a, b, c, d, etc.");
+  includeLowerCasedCharacters = confirm("Would you like to include uppercased characters? For example, A, B, C, D, etc.");
 }
+// Function `getPasswordOptions` returns an object (return statement) with user choices: length & 4 character types 
+  return {
+    length: passwordLength,
+    includeSpecialCharacters: includeSpecialCharacters,
+    includeNumbers: includeNumbers,
+    includeUpperCasedCharacters: includeUpperCasedCharacters,
+    includeLowerCasedCharacters: includeLowerCasedCharacters,
+  };
+}
+userPasswordOptions = getPasswordOptions();
+// console.log(userPasswordOptions);
+ 
 
 // Function for getting a random element from an array
 function getRandom(arr) {
